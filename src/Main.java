0a8f725c02de064f0b17a20a2fe4f5f85dae8713
Boldeusz <mnpower@poczta.onet.pl>
2021-01-18 13:14:42 +0100

    update

--- src/Main.java
+++ src/Main.java
@@ -1,11 +1,21 @@
 public class Main {
     public static void main(String[] args) {
-        calculateScore("Marcin", 500);
+        int newScore = calculateScore("Marcin", 500);
+        System.out.println("New score is " +newScore);
+        calculateScore(75);
+        calculateScore();
     }
 
     public static int calculateScore(String playerName, int score) {
         System.out.println("Player " + playerName + " scored " + score + " points.");
         return score *1000;
     }
-
+    public static int calculateScore(int score) {
+        System.out.println("Unnamed player scored " + score + " points.");
+        return score *1000;
+    }
+    public static int calculateScore() {
+        System.out.println("No player name and no score");
+        return 0;
+    }
 }
